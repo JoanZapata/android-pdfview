318bf1e75f9e7714670058d00c19b0447405795f
Joan Zapata <jzapata@excilys.com>
2013-05-29 22:07:03 +0200

    Changed package name

--- android-pdfview-sample/src/main/java/com/joanzapata/PDFViewActivity.java
+++ android-pdfview-sample/src/main/java/com/joanzapata/PDFViewActivity.java
@@ -7,14 +7,17 @@ import com.actionbarsherlock.view.Menu;
 import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.EActivity;
 import com.googlecode.androidannotations.annotations.ViewById;
+import com.joanzapata.pdfview.sample.R;
 
 @EActivity(R.layout.activity_main)
 public class PDFViewActivity
-    extends SherlockActivity
-{
+        extends SherlockActivity {
 
     @ViewById
     TextView hello;
+
+    @ViewById(R.id.hello)
+    TextView truc;
 
     @AfterViews
     void afterViews() {
