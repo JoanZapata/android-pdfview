d2d51a3f032943baec13a9ad159fa7063a43905e
Joan Zapata <jzapata@excilys.com>
2013-05-29 14:26:36 +0200

    Initialized Android sample project with Android kickstartr #2

---
+++ android-pdfview-sample/src/main/java/com/joanzapata/PDFViewActivity.java
@@ -0,0 +1,29 @@
+
+package com.joanzapata;
+
+import android.widget.TextView;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
+public class PDFViewActivity
+    extends SherlockActivity
+{
+
+    @ViewById
+    TextView hello;
+
+    @AfterViews
+    void afterViews() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater();
+        return true;
+    }
+
+}
