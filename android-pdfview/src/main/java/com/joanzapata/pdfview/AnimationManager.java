c977fb4a614a837e59133a832e214b842a05a96b
Yoav Sternberg <yoav.goop@gmail.com>
2014-06-20 15:12:23 +0300

    change to spaces

--- android-pdfview/src/main/java/com/joanzapata/pdfview/AnimationManager.java
+++ android-pdfview/src/main/java/com/joanzapata/pdfview/AnimationManager.java
@@ -36,83 +36,83 @@ import android.view.animation.DecelerateInterpolator;
  */
 class AnimationManager {
 
-	/**
-	 * PDF View
-	 */
-	private PDFView pdfView;
+    /**
+     * PDF View
+     */
+    private PDFView pdfView;
 
-	private ValueAnimator animation;
+    private ValueAnimator animation;
 
-	public AnimationManager(PDFView pdfView) {
-		this.pdfView = pdfView;
-	}
+    public AnimationManager(PDFView pdfView) {
+        this.pdfView = pdfView;
+    }
 
-	public void startXAnimation(float xFrom, float xTo) {
-		if (animation != null) {
-			animation.cancel();
-		}
-		animation = ValueAnimator.ofFloat(xFrom, xTo);
-		animation.setInterpolator(new DecelerateInterpolator());
-		animation.addUpdateListener(new XAnimation());
-		animation.setDuration(400);
-		animation.start();
-	}
+    public void startXAnimation(float xFrom, float xTo) {
+        if (animation != null) {
+            animation.cancel();
+        }
+        animation = ValueAnimator.ofFloat(xFrom, xTo);
+        animation.setInterpolator(new DecelerateInterpolator());
+        animation.addUpdateListener(new XAnimation());
+        animation.setDuration(400);
+        animation.start();
+    }
 
-	public void startZoomAnimation(float zoomFrom, float zoomTo) {
-		if (animation != null) {
-			animation.cancel();
-		}
-		animation = ValueAnimator.ofFloat(zoomFrom, zoomTo);
-		animation.setInterpolator(new DecelerateInterpolator());
-		ZoomAnimation zoomAnim = new ZoomAnimation();
-		animation.addUpdateListener(zoomAnim);
-		animation.addListener(zoomAnim);
-		animation.setDuration(400);
-		animation.start();
-	}
+    public void startZoomAnimation(float zoomFrom, float zoomTo) {
+        if (animation != null) {
+            animation.cancel();
+        }
+        animation = ValueAnimator.ofFloat(zoomFrom, zoomTo);
+        animation.setInterpolator(new DecelerateInterpolator());
+        ZoomAnimation zoomAnim = new ZoomAnimation();
+        animation.addUpdateListener(zoomAnim);
+        animation.addListener(zoomAnim);
+        animation.setDuration(400);
+        animation.start();
+    }
 
-	public void stopAll() {
-		if (animation != null) {
-			animation.cancel();
-			animation = null;
-		}
-	}
+    public void stopAll() {
+        if (animation != null) {
+            animation.cancel();
+            animation = null;
+        }
+    }
 
-	class XAnimation implements AnimatorUpdateListener {
+    class XAnimation implements AnimatorUpdateListener {
 
-		@Override
-		public void onAnimationUpdate(ValueAnimator animation) {
-			float offset = (Float) animation.getAnimatedValue();
-			pdfView.moveTo(offset, pdfView.getCurrentYOffset());
-		}
+        @Override
+        public void onAnimationUpdate(ValueAnimator animation) {
+            float offset = (Float) animation.getAnimatedValue();
+            pdfView.moveTo(offset, pdfView.getCurrentYOffset());
+        }
 
-	}
+    }
 
-	class ZoomAnimation implements AnimatorUpdateListener, AnimatorListener {
+    class ZoomAnimation implements AnimatorUpdateListener, AnimatorListener {
 
-		@Override
-		public void onAnimationUpdate(ValueAnimator animation) {
-			float zoom = (Float) animation.getAnimatedValue();
-			pdfView.zoomCenteredTo(zoom, new PointF(pdfView.getWidth() / 2, pdfView.getHeight() / 2));
-		}
+        @Override
+        public void onAnimationUpdate(ValueAnimator animation) {
+            float zoom = (Float) animation.getAnimatedValue();
+            pdfView.zoomCenteredTo(zoom, new PointF(pdfView.getWidth() / 2, pdfView.getHeight() / 2));
+        }
 
-		@Override
-		public void onAnimationCancel(Animator animation) {
-		}
+        @Override
+        public void onAnimationCancel(Animator animation) {
+        }
 
-		@Override
-		public void onAnimationEnd(Animator animation) {
-			pdfView.loadPages();
-		}
+        @Override
+        public void onAnimationEnd(Animator animation) {
+            pdfView.loadPages();
+        }
 
-		@Override
-		public void onAnimationRepeat(Animator animation) {
-		}
+        @Override
+        public void onAnimationRepeat(Animator animation) {
+        }
 
-		@Override
-		public void onAnimationStart(Animator animation) {
-		}
+        @Override
+        public void onAnimationStart(Animator animation) {
+        }
 
-	}
+    }
 
 }
